e24cb06f0f4a7d1b5199d8ec3ebac84fc04e135c
daramirezv <da.ramirezv@uniandes.edu.co>
2018-09-20 09:16:36 -0500

    parcial

--- front/src/App.js
+++ front/src/App.js
@@ -1,36 +1,57 @@
 import React, { Component } from 'react';
+import vegaEmbed from 'vega-embed';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state ={
-      municipios:[
-      {dane:40, departamento: "antionquita", municipio:"santa rosa de oso"}
+    this.state = {
+      municipios: [
+        { dane: 40, departamento: "antionquita", municipio: "santa rosa de oso" }
       ]
     };
   }
 
-  componentDidMount(){
-    fetch("/getData")
-    .then((response) => {return response.json();})
-    .then((json)=> this.setState({municipios:json}))
-    .catch((err) => console.log(err));
+  componentDidMount() {
+   
   }
 
-  renderMunicipios()
-  {
-    return this.state.municipios.map((muni) => 
-    <div key = {muni.dane}>{muni.departamento} - {muni.municipio}</div>
-    );
-  }
 
   render() {
+    var yourVlSpec = {
+      "$schema": "https://vega.github.io/schema/vega-lite/v2.0.json",
+      "description": "A simple bar chart with embedded data.",
+      "data": { "url": "data/cars.json" },
+      "mark": "point",
+      "encoding": {
+        "x": { "field": "Horsepower", "type": "quantitative" },
+        "y": { "field": "Miles_per_Gallon", "type": "quantitative" }
+      }
+    }
+
+    this.componentDidMount()
+    {
+      vegaEmbed(this.vid2, yourVlSpec)
+      .then((res) =>  res.view.insert("myData", myData).run())
+    }
+
     return (
       <div className="App">
-        <h1>Consulta Anticorrupcion</h1>
+        <textarea
+          cols="40"
+          rows="20"
+          ref={(div) => this.divTarget = div}>
+        </textarea>
 
-        {this.renderMunicipios()}
+        <button onClick={() => {
+          var obj = {
+            x: "John"
+          };
+
+          this.divTarget.value = JSON.stringify(obj, null, 2);
+        }}>Cambio</button>
+
+        <div ref={(div) => this.vid2 = div}></div>
       </div>
     );
   }
